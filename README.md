268b55c7136171cef7d10854256db5a0fe061564
moznion <moznion@gmail.com>
2016-11-15 18:25:41 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Homebrew repository of [linenotcat](https://github.com/moznion/linenotcat).
+
+Usage
+--
+
+```
+$ brew tap moznion/homebrew-linenotcat
+$ brew install linenotcat
+```
+
---
+++ linenotcat_darwin_amd64.rb
@@ -0,0 +1,39 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class LinenotcatDarwinAmd64 < Formula
+  desc "A command line tool to send messages to LINE Notify"
+  homepage "https://github.com/moznion/linenotcat"
+  url "https://github.com/moznion/linenotcat/releases/download/0.1.0/linenotcat_darwin_amd64_0.1.0"
+  version "0.1.0"
+  sha256 "627e8c8d179a3e981151b97df9bc072d49d299019a7bdc7382c33ffefe3c2026"
+
+  # depends_on "cmake" => :build
+  depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test linenotcat_darwin_amd64`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
